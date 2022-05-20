df921b8e573f00a5ca95e7624b9295b5a9d191f6
faked858 <102006092+faked858@users.noreply.github.com>
2022-05-20 12:17:33 +1200

    Add files via upload

--- Parker Rogers - AS91883 & 91884 project/Code.js
+++ Parker Rogers - AS91883 & 91884 project/Code.js
@@ -1,13 +1,26 @@
-17/05/22
+//author: parker
 
 //get canvas
 window.onload=startCanvas
 //listen for a keypress
 window.addEventListener('keydown', keyFunction)
+window.addEventListener('keyup', keyFunction)
+
 
 var WIDTH = 75
 var HEIGHT = 75
 var ctx
+
+var playerX = 100
+var playerY = 100
+
+var velocityX = 3
+var velocityY = 3
+
+var upPressed = false
+var downPressed = false
+var leftPressed = false
+var rightPressed = false
 
 
 //load canvas
@@ -16,20 +29,40 @@ function startCanvas(){
 	timer = setInterval(updateCanvas, 10)
 }
 
-
+console.log(upPressed)
 
 //this is used to update the canvas every frame, anything that moves must be put in here
 function updateCanvas() {
 	//ctx.fillstyle = "white";
-	ctx.fillRect(0,0, HEIGHT, WIDTH)
-	console.log("frame")
+	ctx.fillRect(playerX, playerY, HEIGHT, WIDTH)
+	///console.log("frame")
 	
+	if(upPressed == true){
+		playerX += velocityX
+	}
+
+
 }
 
 
 //this function is called when a key is pressed 
 function keyFunction(keyboardEvent){
 	var keyPress = keyboardEvent.key
+	
 	///console.log("you pressed ", keyPress)
 	
+	if (keyPress=="w"){
+		upPressed = true
+	}
+	if (keyPress=="a"){
+		leftPressed = true
+	}
+	if (keyPress=="s"){
+		downPressed = true
+	}
+	if (keyPress=="d"){
+		rightPressed = true
+	}
+
+
 } 
