4d02d90777c75f4f836ae4054265487e2dc34b60
faked858 <parker.r.rogers@gmail.com>
2022-05-30 11:00:15 +1200

    collideRevamp

--- game-code-main/Parker Rogers - AS91883 & 91884 project/Code.js
+++ game-code-main/Parker Rogers - AS91883 & 91884 project/Code.js
@@ -16,7 +16,6 @@ var WIDTH = 1000;
 var HEIGHT = 800;
 var ctx
 
-
 var velocityX = 3;
 var velocityY = 3;
 
@@ -34,19 +33,34 @@ var jumpSpeed = 50
 var trueJump = false 
 
 class GameObject{
-	constructor(id,image,x,y,width,height){
+	constructor(id,image,x,y,width,height,pWidth,pHeight){
 		this.id=id;
 		this.image=image
 		this.x=x
 		this.y=y
 		this.width=width
 		this.height=height
+		this.pWidth=75
+		this.pHeight=75
 	}
 
+	collide(){
+		if(this.x + this.pWidth >= WIDTH){
+			this.x = WIDTH - this.pWidth}
+		if(this.x <= WIDTH - WIDTH){
+		this.x = WIDTH - WIDTH}
+		if(this.y + this.pHeight >= HEIGHT){
+			this.y = HEIGHT - this.pHeight}
+		if(this.y >= HEIGHT - HEIGHT){
+			this.y = HEIGHT - HEIGHT
+		}
+	}
+	
 	setPos(x,y){
 		this.x=x
 		this.y=y
 	}
+
 	draw(){
 		ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
 	}
@@ -75,61 +89,27 @@ function updateCanvas() {
 	//ctx.fillRect(player.x, player.y, playerHeight, playerWidth)
 	///console.log("frame")
 	
-	if(rightPressed == true){
-		player.x += velocityX
-	}
-	if(leftPressed == true){
-		player.x -= velocityX
-	}
-	if(downPressed == true){
-		player.y += velocityY
-	}
-	if(upPressed == true){
-		player.y -= velocityY
-	}
+	//calls the player movement functon
+	movementFunction()
 
-	//boundry collison
-	if (player.x + playerWidth >= WIDTH){
-		player.x = WIDTH - playerWidth
-	}
-	if(player.x < WIDTH - WIDTH){
-		player.x = WIDTH - WIDTH
-	}
-	if(player.y + playerHeight >= HEIGHT){
-		player.y = HEIGHT - playerHeight
-	}
-	if(player.y < HEIGHT - HEIGHT){
-		player.y = HEIGHT - HEIGHT
-	}
+	//boundry collison function
+	//collisionFunction()
 		
-	//console.log(velocityY)
-	//gravity
-	if(player.y < HEIGHT - playerHeight && upPressed != true){
-	gravitySpeed += gravity;
-    player.y += velocityY + gravitySpeed;
-	} else{
-		gravitySpeed = 0
-	}
-	//console.log(gravitySpeed)	
+	//gravityFunction
+	gravityFunction()	
 	
 	//jump
-	if(upPressed == true && player.y < HEIGHT - player.height){
-		jumpSpeed -= jump;
-		player.y -= velocityY + jumpSpeed
-		trueJump = true
-	} else{
-		jumpSpeed = 0
-		trueJump = false
-	}
-	//console.log("jumpSpeed is " + jumpSpeed)
+	jumpFunction()
 	
 	//friction
+
+	player.collide()
 }
+
+
+
 	
-if(trueJump == true){
-	upPressed = false
-	console.log("does this work?")
-}
+
 //this function is called when a key is pressed 
 function keyDownFunction(keyboardEvent){
 	var keyPress = keyboardEvent.key
@@ -148,8 +128,6 @@ function keyDownFunction(keyboardEvent){
 	if (keyPress=="d"){
 		rightPressed = true
 	}
-
-
 } 
 
 function keyUpFunction(keyboardEvent){
@@ -159,10 +137,56 @@ function keyUpFunction(keyboardEvent){
 	rightPressed = false
 	downPressed = false
 }
-
-function collide(){
-	if (player.x + playerWidth >= WIDTH){
-		player.x = WIDTH - playerWidth
-	}
-
+//allows player collision with the boundrys, might be redundent later because i need to make a new collision system
+/*function collisionFunction() {
+if (player.x + playerWidth >= WIDTH){
+	player.x = WIDTH - playerWidth
 }
+if(player.x < WIDTH - WIDTH){
+	player.x = WIDTH - WIDTH
+}
+if(player.y + playerHeight >= HEIGHT){
+	player.y = HEIGHT - playerHeight
+}
+if(player.y < HEIGHT - HEIGHT){
+	player.y = HEIGHT - HEIGHT
+}
+}*/
+
+//this is the gravity function, makes gravity work 
+function gravityFunction() {
+if(player.y < HEIGHT - playerHeight && upPressed != true){
+	gravitySpeed += gravity;
+    player.y += velocityY + gravitySpeed;
+	} else{
+		gravitySpeed = 0
+	}
+}
+
+//makes the player move
+function movementFunction(){
+if(rightPressed == true){
+	player.x += velocityX
+}
+if(leftPressed == true){
+	player.x -= velocityX
+}
+if(downPressed == true){
+	player.y += velocityY
+}
+if(upPressed == true){
+	player.y -= velocityY
+}
+}
+
+function jumpFunction() {
+if(upPressed == true && player.y < HEIGHT - player.height){
+	jumpSpeed -= jump;
+	player.y -= velocityY + jumpSpeed
+	trueJump = true
+} else{
+	jumpSpeed = 0
+	trueJump = false
+}
+}
+
